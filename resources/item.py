39ee268937f8a14195607320d59ec4adce91f84a
NilsLandelius <nilsoskar.landelius@gmail.com>
2019-10-10 10:31:36 +0200

    addd a jwt_optional that shows more item data when user is logged in

--- resources/item.py
+++ resources/item.py
@@ -1,12 +1,19 @@
 from flask import request
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
 class ItemList(Resource):
-    def get(self):       
-        return {'items':[item.json() for item in ItemModel.find_all()]} 
+    @jwt_optional
+    def get(self):
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items':items}, 200
+        return {'items':[item['name'] for item in items],
+        'message':'more data available if you log in'
+        }, 200 
 
 class Item(Resource):
         parser = reqparse.RequestParser()
@@ -32,6 +39,7 @@ class Item(Resource):
                 return {'message': 'An error occured inserting the item'}, 500
             
             return item.json(), 201
+        
         @jwt_required
         def delete(self, name):
             claims = get_jwt_claims()
@@ -40,8 +48,8 @@ class Item(Resource):
             item = ItemModel.find_by_name(name)
             if item:
                 item.delete_from_db()
-            
-            return {'message':'Item deleted'}
+                return {'message':'Item deleted'}
+            return {'message':'Item not found'}, 404
                 
 
 
